7a1685679546b6b28ffc829ec76e661372e7896e
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-09-25 10:01:15 -0300

    remove install docker, yay and rust

--- install.sh
+++ install.sh
@@ -2,12 +2,9 @@
 
 sudo pacman -Sy --needed base-devel git --noconfirm && \
 
-bash install-yay.sh && \
 bash arch-packages.sh  && \
-bash arch-docker.sh  && \
 bash install-navi.sh && \
 bash arch-enviroments.sh && \
 bash install-sdkman.sh && \
-bash arch-rust-install.sh && \
 bash arch-packages-browsers.sh
 
