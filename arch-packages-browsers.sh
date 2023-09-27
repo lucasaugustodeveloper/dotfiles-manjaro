0e9cc72f4b3ae275355d751f1a8f24f3028241dd
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2023-09-27 12:06:54 -0300

    add brave and vivaldi browser

--- arch-packages-browsers.sh
+++ arch-packages-browsers.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-yay -S google-chrome opera microsoft-edge-stable-bin --needed --noconfirm
+yay -S google-chrome opera microsoft-edge-stable-bin brave-browser vivaldi --needed --noconfirm
