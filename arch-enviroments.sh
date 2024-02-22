e6bc67fde4579319e10abbd229a61ff8e2682758
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2024-02-22 17:42:36 -0300

    ♻️ Validation of create
    create validation for file exists

--- arch-enviroments.sh
+++ arch-enviroments.sh
@@ -10,7 +10,7 @@ ln -s ~/dotfiles/config/alacritty/ ~/.config/alacritty
 ln -s ~/dotfiles/config/gitmoji-nodejs/ ~/.config/gitmoji-nodejs
 
 # sublime
-ln -s ~/dotfiles/config/sublime/Packages/User/ ~/.config/sublime-text-4/Packages/User
+# ln -s ~/dotfiles/config/sublime/Packages/User/ ~/.config/sublime-text-4/Packages/User
 
 # git
 ln -s ~/dotfiles/config/gitconfig/.gitconfig ~/.gitconfig
@@ -19,18 +19,23 @@ ln -s ~/dotfiles/config/gitconfig/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/config/zsh/.p10k.zsh ~/.p10k.zsh
 
 # ZSH
-if [ -f '~/.zshrc' ]; then mv ~/.zshrc ~/.zshrc.bkp; fi
+if [ $(ls ~/.zshrc) ]; then
+    rm ~/.zshrc
+fi
 ln -s ~/dotfiles/config/zsh/.zshrc ~/.zshrc
 
 # LunarVim
 ln -s ~/dotfiles/config/lvim/config.lua ~/.config/lvim/config.lua
 
 # Tmux
+if [ $(ls ~/.terraformrc); then
+    rm ~/.tmux.conf
+fi
 ln -s ~/dotfiles/config/tmux/.tmux.conf ~/.tmux.conf
 
 #Terraform
-# if [ -f '~/.terraformrc' ]; then mv ~/.terraformrc ~/.terraformrc_original; fi
+# if [ $(ls ~/.terraformrc) ]; then mv ~/.terraformrc ~/.terraformrc_original; fi
 # ln -s ~/dotfiles/config/.terraformrc ~/.terraformrc
 
 # anothers
-mkdir -p ~/bin
+# mkdir -p ~/bin
