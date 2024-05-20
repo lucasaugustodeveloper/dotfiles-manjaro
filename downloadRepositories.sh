7b31171e3c263da968c99befe7738f43aae15ee8
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2024-05-20 09:54:48 -0300

    ♻️ (bash): Change user variables for hardcoded

--- downloadRepositories.sh
+++ downloadRepositories.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#
-echo "Enter username in github:"
-read username
 
-gh repo list $username --limit 200 | while read -r repo _; do
+gh repo list lucasaugustodeveloper --limit 200 | while read -r repo _; do
   gh repo clone "$repo" "$HOME/Repositories/$repo"
 done
 
