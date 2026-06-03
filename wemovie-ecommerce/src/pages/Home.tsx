85db924ccff5e077f088538217a6e9eddae9dc08
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2026-06-03 15:48:01 -0300

    🎉 Add basic structure for app wemovie

---
+++ wemovie-ecommerce/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to WeMovie E-commerce!</h1>
+      <p>Discover the best movies and merchandise here.</p>
+    </div>
+  );
+}
