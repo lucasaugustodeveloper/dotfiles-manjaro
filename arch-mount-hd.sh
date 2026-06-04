090e04327cd7f11bbdd393a4df60cbb628afaf29
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2026-06-04 14:44:49 -0300

    ♻️ Change hardcoded user for user for env

--- arch-mount-hd.sh
+++ arch-mount-hd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-sudo mount -t ntfs-3g -o uid=1000,gid=1000 /dev/sdd1 /home/lucas/HomeMedia && sudo mount -t ntfs-3g -o uid=1000,gid=1000 /dev/sda5 /home/lucas/DownloadBrowser
+sudo mount -t ntfs-3g -o uid=1000,gid=1000 /dev/sdd1 $HOME/HomeMedia && sudo mount -t ntfs-3g -o uid=1000,gid=1000 /dev/sda5 $HOME/DownloadBrowser
