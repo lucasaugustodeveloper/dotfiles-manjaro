aaa12d82eb46fcbb8c7e3589b1b57f8dba352bbe
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2026-07-28 10:27:37 -0300

    🔥 Remove installation of navi

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 sudo pacman -Sy --needed base-devel git --noconfirm && \
 
 bash arch-packages.sh  && \
-bash install-navi.sh && \
 bash arch-enviroments.sh && \
 bash install-sdkman.sh && \
 bash arch-packages-browsers.sh
